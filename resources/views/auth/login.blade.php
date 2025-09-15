78faf324b5b964052a461277e5020b32673b4abd
Aldi Pradana <aldilaboratory@gmail.com>
2025-09-16 04:02:36 +0800

    fix navigation, register, and login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -35,13 +35,18 @@
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+                    {{ __('Lupa password?') }}
                 </a>
             @endif
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
+        </div>
+
+        <hr class="block my-4">
+        <div class="text-center">
+            Belum punya akun? <a href="{{ route('register') }}" style="color: rgb(255, 41, 105)">Daftar di sini.</a>
         </div>
     </form>
 </x-guest-layout>
---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,64 @@
+<nav class="navbar navbar-expand-lg fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#home">Risaa Makeup</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item"><a class="nav-link" href="#home">Beranda</a></li>
+        <li class="nav-item"><a class="nav-link" href="#services">Layanan</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pricing">Package</a></li>
+        <li class="nav-item"><a class="nav-link" href="#gallery">Galeri</a></li>
+      </ul>
+
+      <div class="navbar-actions d-flex align-items-center gap-2">
+        <!-- Cart selalu tampil -->
+        <a href="#" class="nav-icon-btn me-2" title="Shopping Cart">
+          <i class="fas fa-shopping-bag"></i>
+          <span class="cart-count">0</span>
+        </a>
+
+        @guest
+          <!-- Guest: sembunyikan ikon profil, tampilkan tombol Register -->
+          <a href="{{ route('login') }}" class="btn btn-primary btn-sm">
+            Login
+          </a>
+        @endguest
+
+        @auth
+          <!-- Logged in: ikon profil + dropdown -->
+          <div class="dropdown">
+            <a
+              href="#"
+              class="nav-icon-btn d-inline-flex align-items-center"
+              id="profileDropdown"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              title="User Menu"
+            >
+              <i class="fas fa-user"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="profileDropdown">
+              <li>
+                <a class="dropdown-item" href="{{ route('profile.edit') }}">Profile</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">Pesanan</a>
+              </li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <form method="POST" action="{{ route('logout') }}">
+                  @csrf
+                  <button type="submit" class="dropdown-item">Logout</button>
+                </form>
+              </li>
+            </ul>
+          </div>
+        @endauth
+      </div>
+    </div>
+  </div>
+</nav>
